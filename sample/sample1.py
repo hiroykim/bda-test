eb1e23db4d107be951da2eb3b0745db4ef6ea138
hiroykim <kimjunwon@gmail.com>
2021-05-08 23:17:25 +0900

    bda

--- sample/sample1.py
+++ sample/sample1.py
@@ -5,5 +5,11 @@ df = pd.read_csv("data/mtcars.csv")
 #print(df.head())
 
 minmax_scaler = preprocessing.MinMaxScaler().fit(df[['qsec']] )
+print(type(minmax_scaler))
 df_minmax = minmax_scaler.transform(df[["qsec"]])
+print(type(df_minmax))
 print(df_minmax[df_minmax>0.5].shape[0])
+
+minmax_scaler2 = preprocessing.MinMaxScaler().fit_transform(df[['qsec']] )
+print(type(minmax_scaler2))
+print(minmax_scaler2[minmax_scaler2>0.5].shape[0])
