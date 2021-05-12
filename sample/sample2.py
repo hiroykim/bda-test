5a767f3046ec813ee4ad3c9a2d582669e232df09
hiroykim <41009933+hiroykim@users.noreply.github.com>
2021-05-12 13:38:44 +0900

    Add files via upload
    jwkim

--- sample/sample2.py
+++ sample/sample2.py
@@ -1,10 +1,14 @@
 
 
-def sample_1(a, b):
+def sample_1(a=1, b=10, c=100, d=1000):
     print("a : {}".format(a))
     print("b : {}".format(b))
+    print("c : {}".format(c))
+    print("d : {}".format(d))
 
 
 if __name__=="__main__":
+    sample_1()
     sample_1(a=1, b=2)
-    sample_1(1, b=2)
+    sample_1(1, 2, 10, 100)
+    sample_1(a=1, b=2, c=10, d=100)
