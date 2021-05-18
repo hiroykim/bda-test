17399521be207cffdba60bde6594edeb23f24726
hiroykim <kimjunwon@gmail.com>
2021-05-18 09:31:33 +0900

    jwkim

--- sample/sample3.py
+++ sample/sample3.py
@@ -27,11 +27,33 @@ def sample_4(a,b,c, **kargs):
     print(kargs.get("key3"))
 
 
+def sample_5(*args,**kargs):
+    print(kargs)
+    print(*kargs)
+    #print(**kargs)
+    print(args)
+    print(args[0]+1,args[1]+1,args[2]+1)
+    print(*args)
+
+
+def sample_6(s=-99, *args,**kargs):
+    print(kargs)
+    print(*kargs)
+    #print(**kargs)
+    print(args)
+    #print(args[0]+1,args[1]+1)
+    print(*args)
+    print("s:", s)
+
+
 if __name__=="__main__":
     options = {"key1": "data1", "key2":"data2", "key3":"data3"}
     sample_1(options)
     sample_2(options)
     #sample_3(options)
-    print("---------------------------------")
     sample_3(11, 22, 33, key1="data1", key2="data2", key3="data3")
+
+    sample_5(11, 22, 33, key1="data1", key2="data2", key3="data3")
+    print("--------------------------------->sample6666666666")
+    sample_6(key1="data1", key2="data2", key3="data3")
 
