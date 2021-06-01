78160c5cbceb32e97a963d17278191f64cddfbcc
hiroykim <kimjunwon@gmail.com>
2021-06-01 12:46:29 +0900

    jwkim

---
+++ src/basecollect.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from urllib.parse import quote_plus
+from selenium import webdriver
+import pandas as pd
+
+base_url= 'https://www.google.com/search?q='
+plus_url=input('keyword : ')
+url= base_url + quote_plus(plus_url)
+
+driver= webdriver.Chrome("../install/chromedriver")
+
+driver.get(url)
+
