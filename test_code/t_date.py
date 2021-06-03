6a35d5d8e748684e0911e6f48aade87ad9edb327
hiroykim <kimjunwon@gmail.com>
2021-06-03 15:23:10 +0900

    jwkim

---
+++ test_code/t_date.py
@@ -0,0 +1,18 @@
+import time
+import datetime
+from datetime import timedelta
+
+
+print(time.time())
+print(time.localtime())
+print(time.localtime(time.time()))
+
+#https://dojang.io/mod/page/view.php?id=2463
+date = time.strftime('%Y%m%d%H%M%S', time.localtime())
+print(date)
+
+print(datetime.datetime(2020,2,2))
+print(datetime.datetime.today())
+today = datetime.datetime.today()
+d_day = today - timedelta(days=30)
+print(d_day)
