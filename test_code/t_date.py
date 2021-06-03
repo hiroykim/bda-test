88946e7fd3887b5dafa65cc99f19296e081c2eac
hiroykim <kimjunwon@gmail.com>
2021-06-03 16:38:56 +0900

    jwkim

--- test_code/t_date.py
+++ test_code/t_date.py
@@ -1,18 +1,25 @@
 import time
 import datetime
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 print(time.time())
 print(time.localtime())
 print(time.localtime(time.time()))
+st = time.localtime()
+print(st.tm_year)
 
+# 날짜 포맷 사용
 #https://dojang.io/mod/page/view.php?id=2463
 date = time.strftime('%Y%m%d%H%M%S', time.localtime())
 print("YYYYMMDDhh24mmss :", date)
 
-print(datetime.datetime(2020,2,2))
-print(datetime.datetime.today())
-today = datetime.datetime.today()
+# 시간 이동 사용
+print(datetime(2020,2,2))
+print(datetime.today())
+today = datetime.today()
 d_day = today - timedelta(days=30)
 print(d_day)
+
+# db timestamp 사용
+print("timestamp: ", datetime.fromtimestamp(time.time()))
