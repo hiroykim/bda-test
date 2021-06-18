dc12ff40a004873a15c5c89574adbfe40297f56a
hiroykim <kimjunwon@gmail.com>
2021-06-18 14:46:09 +0900

    bda

--- src/collect_1.py
+++ src/collect_1.py
@@ -1,2 +1,13 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import requests
+
+url = "http://www.naver.com"
+
+param ={ "aa": 'bb'}
+response = requests.get(url, params=param)
+
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+text = soup.find_all("text")
