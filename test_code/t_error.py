49623faca28ba1ba6872e274c627ed5baa99c643
hiroykim <kimjunwon@gmail.com>
2021-06-23 20:21:29 +0900

    bda

--- test_code/t_error.py
+++ test_code/t_error.py
@@ -1,15 +1,24 @@
 import traceback
 
+def test():
+    test_err("test")
+
 def test_err(msg):
     try:
         print("try {0}".format(msg))
         1/0
     except:
         print("func error")
-        raise Exception()
+        raise Exception("hahahja error")
+
+def test_err2(msg):
+    print("try1 {0}".format(msg))
+    1/0
+    print("try2 {0}".format(msg))
+
 
 if __name__=="__main__":
     try:
-        test_err("msg")
+        test()
     except:
         print("main func : {0}".format(traceback.format_exc()))
